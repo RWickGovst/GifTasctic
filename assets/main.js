3a537378a4a803500429b7d60aee6e18c32233e0
Triumph <archie200@icloud.com>
2019-06-11 08:51:31 -0500

    set up html and some js functionality

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+var topics = ["Sportbike", "Cruiser", "Touring"];
+
+function renderButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var topicBtn = $("<button>");
+        topicBtn.text(topics[i])
+        topicBtn.addClass("topic-btn");
+        topicBtn.attr("data-topicname", topics[i]);
+        $("#buttons-view").append(topicBtn)
+
+      }
+    }
+    $(document).on("click", ".topic-btn", function (event) {
+        var topicName = $(this).attr("data-topicname");
+        
+        var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=uLApXoKhCJXtCH2lOlPkxCj2HQxXeeTf";
+
+        // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function (response) {
+        
+
+        });
+    });
+
+    $("#add-topic").on("click", function (event) {
+        var userTopic = $("#topic-input").val();
+        topics.push(userTopic);
+        event.preventDefault();
+        renderButtons();
+        });
+
+    renderButtons();
